97b34cb1b2758b3e3a3775719780d8895798fe47
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-07 20:47:48 +0500

    Photo classi yaratildi

---
+++ PhotoProject/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoProject
+{
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+        public void Save(string path) { }
+    }
+}
