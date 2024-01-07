2d8d8cedb0bd508f8fe0245cde1372724de6bf5c
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-07 20:50:44 +0500

    Updated qilindi

---
+++ PhotoProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using PhotoProject;
+
+namespace DelegateMasala2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBringthness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += filters.Resize;
+            processor.Process("photo.jpg", filterHandler);
+        }
+    }
+}
