3902643d76690772950ef870597810ff12459897
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-08 02:17:37 +0500

    Premium userlarni saralovchi PremiumUsers sinfi yaratildi

---
+++ ShoppingCartDelegate/PremiumUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDelegate
+{
+    public class PremiumUser: IUser
+    {
+        public string FullName {  get; set; }
+        public ShoppingCart Cart { get; set; }
+        public decimal GetPriceDiscountForUser(decimal totalPrice)
+        {
+            return totalPrice * (1 - 0.30M);
+        }
+    }
+}
