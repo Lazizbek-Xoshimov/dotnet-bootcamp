d30abedf6b8e61c61777608c47a8e331077c876e
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-07 20:59:34 +0500

    Delegatega oid misol yozilindi

---
+++ DelegategaMisol/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DelegateMisol
+{
+    public class Program
+    {
+        public delegate int Calculation(int a, int b);
+        public static void Main(string[] args)
+        {
+            Calculation calculate = new Calculation(Multiplication);
+            Console.WriteLine(calculate.Invoke(6, 7));
+        }
+        private static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
