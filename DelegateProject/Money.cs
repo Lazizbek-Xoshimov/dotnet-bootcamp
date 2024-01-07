011d75982638cfc087d803421129e57be0eaa0b6
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-07 20:32:37 +0500

    Money classi yaratildi

---
+++ DelegateProject/Money.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProject
+{
+    public class Money
+    {
+        public decimal Amount { get; set; } = 0;
+        public string Currency { get; set; } = "";
+        public delegate void MoneyDelegate(Money money);
+
+        public Money(decimal Amount)
+        {
+            this.Amount = Amount;
+            this.Currency = "UZB";
+        }
+
+    }
+}
