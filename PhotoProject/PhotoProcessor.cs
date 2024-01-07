93c8f8b854c83d3ebca31adcc1c3813c56b5a498
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-07 20:49:58 +0500

    PhotoProcessor classi yaratildi

---
+++ PhotoProject/PhotoProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoProject
+{
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+        public void Process(string path,  PhotoFilterHandler handler)
+        {
+            var photo = Photo.Load(path);
+            handler(photo);
+        }
+    }
+}
