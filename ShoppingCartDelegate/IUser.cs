c86792a74216d584237b1ff98b2ea0471f683f80
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-08 02:16:39 +0500

    IUser nomli user sinfi yaratildi

---
+++ ShoppingCartDelegate/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDelegate
+{
+    public interface IUser
+    {
+        string FullName { get; set; }
+        ShoppingCart Cart { get; set; }
+        decimal GetPriceDiscountForUser(decimal totalPrice);
+    }
+}
