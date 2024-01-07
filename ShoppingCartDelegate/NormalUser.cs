9cc82271e8d0aae99180e82127afd7c2323c7160
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-08 02:17:08 +0500

    NormalUsers nomli sinf yaratildi

---
+++ ShoppingCartDelegate/NormalUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDelegate
+{
+    public class NormalUser: IUser
+    {
+        public string FullName {  get; set; }
+        public ShoppingCart Cart { get; set; }
+        public decimal GetPriceDiscountForUser(decimal totalPrice)
+        {
+            if(Cart.Products.Count > 5) 
+            { 
+                return totalPrice * (1-0.30M);
+            }
+            else
+            {
+                return totalPrice;
+            }
+        }
+    }
+}
