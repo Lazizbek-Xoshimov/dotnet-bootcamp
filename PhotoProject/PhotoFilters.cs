308affef037299187d7a1ce9a61995ec5c50f84a
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-07 20:48:05 +0500

    PhotoFilters classi yarratildi

---
+++ PhotoProject/PhotoFilters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoProject
+{
+    public class PhotoFilters
+    {
+        public void ApplyBringthness(Photo photo)
+        {
+            Console.WriteLine("Apply Bringthness. ");
+        }
+        public void ApplyContrast(Photo photo) 
+        {
+            Console.WriteLine("Apply Contrast. ");
+        }
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Apply Resize. ");
+        }
+    }
+}
