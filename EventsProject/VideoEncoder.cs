caae8d6c765e44c846e427a29d3b7600687599d6
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-10 01:08:21 +0500

    VideoEncoder publisher classi yaratildi

---
+++ EventsProject/VideoEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsProject
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    public class VideoEncoder
+    {
+        //public delegate void VideoEncoderHandler(object source, VideoEventArgs args);
+
+        //public event VideoEncoderHandler VideoEncoded;
+
+        //public event EventHandler VideoEncoded;
+
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public virtual void OnVideoEncoded(Video video)
+        {
+            if(VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video});
+            }
+        }
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding a video... ");
+            Thread.Sleep(3000);
+            OnVideoEncoded(video);
+        }
+    }
+}
