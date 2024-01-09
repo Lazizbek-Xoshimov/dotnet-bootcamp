078a5ec7a768e9be93e6059f66da1f38c5fc5732
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-10 01:07:57 +0500

    InstantMessageService subscribe classi yaratildi

---
+++ EventsProject/InstantMessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsProject
+{
+    public class InstantMessageService
+    {
+        public void OnVideoEncodedForMessage(object source, VideoEventArgs args)
+        {
+            Console.WriteLine("InstantMessageService: Sending a message... " + args.Video.Title);
+        }
+    }
+}
