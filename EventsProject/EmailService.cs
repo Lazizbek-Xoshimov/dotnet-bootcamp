b89065c1fc9796cf8afb7950f010c3db610e1ebc
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-10 01:07:26 +0500

    EmailService subscribe classi yaratildi

---
+++ EventsProject/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsProject
+{
+    public class EmailService
+    {
+        public void OnVideoEncodedForEmail(object source, VideoEventArgs args)
+        {
+            Console.WriteLine("Email Service: Sending an email... " + args.Video.Title);
+        }
+    }
+}
