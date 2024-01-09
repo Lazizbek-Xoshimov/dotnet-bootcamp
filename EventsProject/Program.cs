6be56e64e4c886a9342236daff0cc522b6a8bfaa
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-10 01:08:59 +0500

    Updated.

---
+++ EventsProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using EventsProject;
+
+namespace Events
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var video = new Video() {Title = "Birinchi video." };
+            var videoEncoder = new VideoEncoder();
+            var mailService = new EmailService();
+            var messageService = new InstantMessageService();
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncodedForEmail;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncodedForMessage;
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
