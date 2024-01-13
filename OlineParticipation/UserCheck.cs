745383aab9732884806956ee522aa720b1a0350f
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-13 05:36:00 +0500

    UserCheck nomli publisher class qo'shildi

---
+++ OlineParticipation/UserCheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlineParticipation
+{
+    //public class UserCheckArgs: EventArgs
+    //{
+    //    public User User { get; set; }
+    //}
+    public class UserCheck
+    {
+        public delegate void UserCheckHendlerDelegate(User user);
+        public event UserCheckHendlerDelegate UserCheckHendler;
+
+        public virtual void OnUserCheck(User user)
+        {
+            if(UserCheckHendler != null)
+            {
+                //UserCheckHendler(this, new UserCheckArgs() { User = user});
+                UserCheckHendler(user);
+            }
+        }
+
+        public void Check(User user)
+        {
+            Console.Write($"{user.Name} ");
+            OnUserCheck(user);
+        }
+    }
+}
