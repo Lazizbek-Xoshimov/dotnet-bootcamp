b4b1be4f468c40a40d30c9b28955750ee1f8bdb4
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-13 05:36:27 +0500

    Incpestion nomli subscriber class qo'shildi

---
+++ OlineParticipation/Incpestion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlineParticipation
+{
+    public class Incpestion
+    {
+        public string[] names = { "Lazizbek", "Nurmurod", "Zohidbek" };
+        public void OnCheckIncpestion(User user)
+        {
+            bool Lamp = false;
+            foreach (var name in names)
+            {
+                if(user.Name == name)
+                {
+                    Console.WriteLine("kirishingiz taqiqlangan! ");
+                    Lamp = false;
+                    break;
+                }
+                else Lamp = true;
+            }
+            if(Lamp) 
+                Console.WriteLine("xush kelibsiz! ");
+        }
+    }
+}
