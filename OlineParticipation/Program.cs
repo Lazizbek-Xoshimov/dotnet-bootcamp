b7796005232ed7d4428bc3260699706b6b0258da
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-13 05:36:46 +0500

    Updated.

---
+++ OlineParticipation/Program.cs
@@ -0,0 +1,18 @@
+﻿using OlineParticipation;
+using System;
+namespace OnlineParticipation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User { Name = Console.ReadLine() };
+            var incpestion = new Incpestion();
+            var userCheck = new UserCheck();
+
+            userCheck.UserCheckHendler += incpestion.OnCheckIncpestion;
+
+            userCheck.Check(user);
+        }
+    }
+}
