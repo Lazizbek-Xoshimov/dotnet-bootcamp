4eeb2ff83af6b34ed49d89e00661250ec1343795
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-26 02:52:21 +0500

    IndexerXosilQilish nomli indexer class yaratildi.

---
+++ IndexerInCSharp/IndexerXosilQilish.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerInCSharp
+{
+    public class IndexerXosilQilish
+    {
+        private int[] massiv = new int[4];
+
+        public int this[int index]
+        {
+            get
+            {
+                return massiv[index];
+            }
+            set
+            {
+                massiv[index] = value;
+            }
+        }
+    }
+}
