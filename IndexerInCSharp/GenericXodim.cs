f077cb9b988b739e529701782e2588e043477f49
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-26 02:53:00 +0500

    GenericXodim nomli generic indexer class yaratildi.

---
+++ IndexerInCSharp/GenericXodim.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerInCSharp
+{
+    public class GenericXodim<T>
+    {
+        private T[] genericxodim = new T[9];
+        public T this[int index]
+        {
+            get
+            {
+                return genericxodim[index];
+            }
+            set
+            {
+                genericxodim[index] = value;
+            }
+        }
+    }
+}
