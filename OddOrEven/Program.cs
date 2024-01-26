0fe6bbb90bd4a49b58d464fca8a430dfaa1f553b
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-01-26 23:48:44 +0500

    Updated.

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OddOrEven
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            int numberInt = int.Parse(Console.ReadLine());
+            Console.Write("Enter an float: ");
+            float numberFloat = float.Parse(Console.ReadLine());
+            Console.Write("Enter an double: ");
+            double numberDouble = double.Parse(Console.ReadLine()); 
+
+            numberInt.OddOrEven();
+            numberFloat.OddOrEven();
+            numberDouble.OddOrEven();
+        }
+    }
+}
