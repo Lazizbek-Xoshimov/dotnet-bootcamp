b036fe69a88ecf74e9cb7166c01d3cb8278e4249
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-02-14 15:01:45 +0500

    Created MoviesDbContext

---
+++ MoviesWebApi/Context/MoviesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWebApi.Models;
+
+namespace MoviesWebApi.Context
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
