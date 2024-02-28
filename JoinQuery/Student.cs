7830ac9309c47d327b1ea3e50b4bce8646d3883f
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-02-28 21:07:26 +0500

    Created student model

---
+++ JoinQuery/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace JoinQuery
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Major {  get; set; }
+        public int UniversityId {  get; set; }
+        public University University { get; set; }
+    }
+}
