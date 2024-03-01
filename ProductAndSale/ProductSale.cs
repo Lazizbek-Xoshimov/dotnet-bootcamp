784b6e70f1ac3bbf049db26e0042d2f5f034e8cd
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-03-02 04:44:46 +0500

    LINQ: Join, GroupJoin, SelectMany, LeftOuterJoin

---
+++ ProductAndSale/ProductSale.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAndSale
+{
+    public class ProductSale
+    {
+        public int? ProductId { get; set; }
+        public int? SaleId { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ ProductAndSale/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAndSale
+{
+    public class Sale
+    {
+        public int? Id { get; set; }
+        public decimal? Price { get; set; }
+        public int? ProductId { get; set; }
+        public Product Product { get; set; }
+        public string ProductColor { get; set; }
+    }
+}
