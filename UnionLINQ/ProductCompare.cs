65ed08ab44038f464ff8f4301ff26a6f47658fc9
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2024-03-02 17:22:10 +0500

    LINQ: Union, AggregateFunction, SequenceEqual, Last, ExceptBy, Contains, IntersectBy

---
+++ UnionLINQ/ProductCompare.cs
@@ -0,0 +1,17 @@
+﻿using ProductAndSale;
+using System.Diagnostics.CodeAnalysis;
+
+public class ProductCompare : EqualityComparer<Product>
+{
+    public override bool Equals(Product? product, Product? anotherProduct)
+    {
+        return (product?.Id == anotherProduct?.Id &&
+            product?.Name == anotherProduct?.Name &&
+            product?.Color == anotherProduct?.Color);
+    }
+
+    public override int GetHashCode([DisallowNull] Product product)
+    {
+        return $"{product.Id}{product.Name}{product.Color}".GetHashCode();
+    }
+}
---
+++ UnionLINQ/ProductIdComparer.cs
@@ -0,0 +1,17 @@
+﻿using ProductAndSale;
+using System.Diagnostics.CodeAnalysis;
+
+namespace UnionLINQ
+{
+    public class ProductIdComparer : EqualityComparer<Product>
+    {
+        public override bool Equals(Product? product, Product? anotherProduct)
+        {
+            return product?.Id == anotherProduct?.Id;
+        }
+        public override int GetHashCode([DisallowNull] Product product)
+        {
+            return product.Id.GetHashCode();
+        }
+    }
+}
